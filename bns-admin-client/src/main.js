35a55430852e12ea3bbd56f8aebfb8172ac5d725
jojoldu <jojoldu@gmail.com>
2018-11-12 15:00:16 +0900

    vue 모듈 등록

---
+++ bns-admin-client/src/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jojoldu@gmail.com on 2018. 11. 12.
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
